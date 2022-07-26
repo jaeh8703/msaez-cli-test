304a571b34f81df8b7d77cccca391637215fdb3e
jaeh8703 <65217813+jaeh8703@users.noreply.github.com>
2022-07-26 14:51:15 +0900

    commit msg

---
+++ pet/src/main/java/petstore/domain/Pet.java
@@ -0,0 +1,86 @@
+package petstore.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import petstore.PetApplication;
+import petstore.domain.PetRegistered;
+
+@Entity
+@Table(name = "Pet_table")
+@Data
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Embedded
+    private Photo photo;
+
+    private String name;
+
+    private Integer energy;
+
+    private Integer appearance;
+
+    private Integer weight;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @ElementCollection
+    private List<IllnessHistory> illnessHistory;
+
+    @PostPersist
+    public void onPostPersist() {
+        PetRegistered petRegistered = new PetRegistered(this);
+        petRegistered.publishAfterCommit();
+    }
+
+    public static PetRepository repository() {
+        PetRepository petRepository = PetApplication.applicationContext.getBean(
+            PetRepository.class
+        );
+        return petRepository;
+    }
+
+    public void feed123() {
+        //
+    }
+
+    public void feed123aaaa() {
+        //
+    }
+
+    public void combaaaaaasd123() {
+        //
+    }
+
+    public void c123() {
+        //
+    }
+
+    public void test2() {
+        //
+    }
+
+    public void feed() {
+        Overweighted overweighted = new Overweighted(this);
+        overweighted.publishAfterCommit();
+
+        Fed fed = new Fed(this);
+        fed.publishAfterCommit();
+    }
+
+    public void groom() {}
+
+    public void comb() {}
+}
