6702f8482c44c4474460c98e919fa2bbb17cddda
jaeh8703 <65217813+jaeh8703@users.noreply.github.com>
2022-07-26 16:44:41 +0900

    commit msg

---
+++ frontend/src/components/Test12Command.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            Test12
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="test12"
+            >
+                Test12
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'Test12Command',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+        },
+        watch: {
+        },
+        methods: {
+            test12() {
+                this.$emit('test12', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
