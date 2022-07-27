b4c798d98b5dcdbc77a48fe6b74d7ac90dadae17
jaeh8703 <65217813+jaeh8703@users.noreply.github.com>
2022-07-27 12:06:45 +0900

    commit msg

--- frontend/src/components/Pet.vue
+++ frontend/src/components/Pet.vue
@@ -106,6 +106,14 @@
             >
                 Testa2
             </v-btn>
+            <v-btn
+                    v-if="!editMode"
+                    color="deep-purple lighten-2"
+                    text
+                    @click="testa3"
+            >
+                Testa3
+            </v-btn>
         </v-card-actions>
 
         <v-snackbar
@@ -302,6 +310,21 @@
                 try {
                     if(!this.offline) {
                         var temp = await axios.put(axios.fixUrl(this.value._links.testa2.href))
+                        for(var k in temp.data) {
+                            this.value[k]=temp.data[k];
+                        }
+                    }
+
+                    this.editMode = false;
+                } catch(e) {
+                    this.snackbar.status = true
+                    this.snackbar.text = e
+                }
+            },
+            async testa3() {
+                try {
+                    if(!this.offline) {
+                        var temp = await axios.put(axios.fixUrl(this.value._links.testa3.href))
                         for(var k in temp.data) {
                             this.value[k]=temp.data[k];
                         }
--- pet/src/main/java/petstore/infra/PetController.java
+++ pet/src/main/java/petstore/infra/PetController.java
@@ -122,5 +122,26 @@ public class PetController {
         petRepository.save(pet);
         return pet;
     }
+
+    @RequestMapping(
+        value = "pets/{id}/testa3",
+        method = RequestMethod.PUT,
+        produces = "application/json;charset=UTF-8"
+    )
+    public Pet testa3(
+        @PathVariable(value = "id") Long id,
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws Exception {
+        System.out.println("##### /pet/testa3  called #####");
+        Optional<Pet> optionalPet = petRepository.findById(id);
+
+        optionalPet.orElseThrow(() -> new Exception("No Entity Found"));
+        Pet pet = optionalPet.get();
+        pet.testa3();
+
+        petRepository.save(pet);
+        return pet;
+    }
     // keep
 }
